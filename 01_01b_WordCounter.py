f8c483bbd66077ef0b80eca4e378cca764ef1ab3
ArpithaSrinivas <arpitha.srinivas@sjsu.edu>
2023-01-23 21:51:27 +0000

    updated wordcount logic and paragraph

--- 01_01b_WordCounter.py
+++ 01_01b_WordCounter.py
@@ -1,9 +1,18 @@
+import string
+from collections import Counter
+
+
 def count_words(paragraph):
-    #code goes here
+    paragraph = paragraph.lower()
+    paragraph = paragraph.translate(str.maketrans(" ", " ", string.punctuation))
+    wordList = paragraph.split()
+    counter = Counter(wordList)
+    return counter
     return
 
+
 def main():
-    paragraph = """Nadia’s Garden Restaurant is the creation of husband and wife team Nadia and Timothy Arbore. 
+    paragraph = """Nadia’s nadia nadia Garden Restaurant is the creation of husband and wife team Nadia and Timothy Arbore. 
     Their American-infused, Italian-based, organically created, cuisine was inspired by Nadia’s papa, an immigrant from Italy, 
     who shared his love of cooking with Nadia as a young girl. His focus on using fresh ingredients and family style dining were 
     the inspiration for Nadia’s Garden Restaurant. Located in the heart of Main Streets Historic District, they are proud to be a 
@@ -11,7 +20,8 @@ def main():
      that could be made from local organic suppliers. Preservation of the building’s original layout has allowed them to create smaller, 
      more intimate, dining spaces. Nadia and Timothy are committed to sharing their family history of cuisine, along with their new inspirations,
       with their customers. Their passion for community, entertainment, and hospitality are found in every aspect of Nadia’s Garden Restaurant."""
-    print(count_words(paragraph)) 
+    print(count_words(paragraph))
+
 
 if __name__ == "__main__":
     main()
